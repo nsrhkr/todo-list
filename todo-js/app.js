c70010ae9dfafa5662824f647fd95a7279723601
nsrhkr <hn1769@yandex.com>
2021-05-29 20:24:39 +0900

    イベントの取得方法をEventListenerに変更

--- todo-js/app.js
+++ todo-js/app.js
@@ -18,13 +18,43 @@ class TodoItem {
    */
   createTodo() {
     this.id = this.generateId(3); // idを生成（idが重複した場合3回まで再生成）
-    return `<div id="${this.id}" class="l-item md-item">
-<div class="l-item-checkbox"><input type="checkbox" onclick="changeStatus(${this.id})"></div>
-<div class="l-item-text">
-<span>${this.todoText}</span>
-</div>
-<div class="l-item-delete"><button onclick="remove(${this.id})">削除</button></div>
-</div>`;
+
+    // 完了チェックボックス
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    // チェックイベント登録
+    checkbox.addEventListener('change', (event) => {changeStatus(event);});
+    // 完了チェックボックスの親
+    const checkboxArea = document.createElement('div');
+    checkboxArea.className = 'l-item-checkbox';
+    checkboxArea.appendChild(checkbox);
+
+    // タスク名表示部
+    const span = document.createElement('span');
+    span.appendChild(document.createTextNode(this.todoText));
+    // タスク名表示部の親
+    const textarea = document.createElement('div');
+    textarea.className = 'l-item-text'
+    textarea.appendChild(span);
+
+    // 削除ボタン
+    const deleteButtonArea = document.createElement('div');
+    deleteButtonArea.className = 'l-item-delete';
+    // 削除ボタンの親
+    const deleteButton = document.createElement('button');
+    deleteButton.appendChild(document.createTextNode('削除'));
+    // 削除イベント登録
+    deleteButton.addEventListener('click', (event) => {remove(event);});
+    deleteButtonArea.appendChild(deleteButton);
+
+    // TODOアイテムの一番外側の要素
+    const box = document.createElement('div');
+    box.id = this.id
+    box.classList.add('l-item', 'md-item');
+    box.appendChild(checkboxArea);
+    box.appendChild(textarea);
+    box.appendChild(deleteButtonArea);
+    return box;
   }
 
   /**
@@ -65,7 +95,8 @@ const submit = () => {
   // TODOアイテムのhtml要素(テキスト)を用意
   const todoItem = new TodoItem(input.value);
   // TODOリストの先頭に挿入
-  todoList.insertAdjacentHTML('afterbegin', todoItem.todoHtml);
+  let theFirstChild = todoList.firstChild
+  todoList.insertBefore(todoItem.todoHtml, theFirstChild);
   // テキストボックスの中身をリセット
   input.value = '';
   console.log(`New TODO: ${todoItem.todoText}`);
@@ -74,23 +105,27 @@ const submit = () => {
 /**
  * TODOアイテムのステータス(色)を変更する
  * 完了(グレー)⇔未完了(ホワイト)
- * @param {string} id アイテムの一番外側のhtml要素のid
+ * @param {object} event チェックボックスの値変更イベント
  * @return 無し
  */
-const changeStatus = (id) => {
-  const todo = document.getElementById(id);
-  const todoCheckbox = todo.querySelector('input');
+const changeStatus = (event) => {
+  // TODOアイテムの一番外側の要素を取得
+  // 注意：DOMの構造を変更した場合は適切に取得できない可能性がある
+  const todo = event.path[2];
+  const todoCheckbox = event.path[0];
   todo.style.backgroundColor = todoCheckbox.checked ? 'lightgray' : 'white';
   console.log(`Change Status`);
 }
 
 /**
  * TODOアイテムを削除
- * @param {string} id アイテムの一番外側のhtml要素のid
+ * @param {object} event 削除ボタンのクリックイベント
  * @return 無し
  */
-const remove = (id) => {
-  const todo = document.getElementById(id);
+const remove = (event) => {
+  // TODOアイテムの一番外側の要素を取得
+  // 注意：DOMの構造を変更した場合は適切に取得できない可能性がある
+  const todo = event.path[2];
   while (todo.firstChild) {
     todo.removeChild(todo.firstChild);
   }
